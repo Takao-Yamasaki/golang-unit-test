fb7c08500a5ce43b96c22bf8528f2128a8c52533
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-02-18 20:26:00 +0900

    add:copycalculatorの作成

---
+++ copycalculator/calculator.go
@@ -0,0 +1,25 @@
+package copycalculator
+
+// 割引計算機
+type DiscountCalculator struct {
+	minimumPurchaseAmount int // 最低購入金額
+	discountAmount        int // 割引額
+}
+
+// 割引を適用するための最低購入額と割引額を受け取る
+func NewDiscountCalculator(minimumPurchaseAmount int, discountAmount int) *DiscountCalculator {
+	return &DiscountCalculator{
+		minimumPurchaseAmount: minimumPurchaseAmount,
+		discountAmount:        discountAmount,
+	}
+}
+
+func (c *DiscountCalculator) Calculate(purchaseAmount int) int {
+
+	// 購入金額が最低購入金額よりも高い場合は割引が適用される
+	if purchaseAmount > c.minimumPurchaseAmount {
+		return purchaseAmount - c.discountAmount
+	}
+	// それ以外は購入金額を返す
+	return purchaseAmount
+}
---
+++ copycalculator/calculator_test.go
@@ -0,0 +1,23 @@
+package copycalculator
+
+import "testing"
+
+// 割引が適用される場合のテスト
+func TestDiscountApplied(t *testing.T) {
+	calculator := NewDiscountCalculator(100, 20)
+	amount := calculator.Calculate(150)
+
+	if 130 != amount {
+		t.Fail()
+	}
+}
+
+// 割引が適用されない場合のテスト
+func TestDiscountNotApplied(t *testing.T) {
+	calculator := NewDiscountCalculator(100, 20)
+	amount := calculator.Calculate(50)
+
+	if 50 != amount {
+		t.Fail()
+	}
+}
