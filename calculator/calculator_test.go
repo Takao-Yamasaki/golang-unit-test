21bdf5448653f981a34b2d3243a47349694128e6
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-02-19 11:22:56 +0900

    add:テスト名の追加

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -5,6 +5,7 @@ import "testing"
 // テーブルドリブンテスト
 func TestDiscountCalculator(t *testing.T) {
 	type testCase struct {
+		name                  string
 		minimumPurchaseAmount int
 		discount              int
 		purchaseAmount        int
@@ -12,15 +13,15 @@ func TestDiscountCalculator(t *testing.T) {
 	}
 
 	testCases := []testCase{
-		{minimumPurchaseAmount: 100, discount: 20, purchaseAmount: 150, expectedAmount: 130},
-		{minimumPurchaseAmount: 100, discount: 20, purchaseAmount: 200, expectedAmount: 160},
-		{minimumPurchaseAmount: 100, discount: 20, purchaseAmount: 350, expectedAmount: 290},
-		{minimumPurchaseAmount: 100, discount: 20, purchaseAmount: 50, expectedAmount: 40},
+		{name: "should apply 20", minimumPurchaseAmount: 100, discount: 20, purchaseAmount: 150, expectedAmount: 130},
+		{name: "should apply 40", minimumPurchaseAmount: 100, discount: 20, purchaseAmount: 200, expectedAmount: 160},
+		{name: "should apply 60", minimumPurchaseAmount: 100, discount: 20, purchaseAmount: 350, expectedAmount: 290},
+		{name: "should not apply", minimumPurchaseAmount: 100, discount: 20, purchaseAmount: 50, expectedAmount: 50},
 	}
 
 	for _, tc := range testCases {
 		// サブテスト
-		t.Run("discount test", func(t *testing.T) {
+		t.Run(tc.name, func(t *testing.T) {
 			calculator := NewDiscountCalculator(tc.minimumPurchaseAmount, tc.discount)
 			amount := calculator.Calculate(tc.purchaseAmount)
 			if amount != tc.expectedAmount {
