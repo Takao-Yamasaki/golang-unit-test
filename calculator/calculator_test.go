aa7a9bb94f427fc140715b96f25c07d2825bdf80
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-02-19 16:56:35 +0900

    add:Counterの実装

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -6,11 +6,16 @@ import (
 )
 
 type DiscountRepositoryMock struct {
-	DiscountValue int
+	Couner int
 }
 
 func (drm DiscountRepositoryMock) FindCurrentDiscount() int {
-	return drm.DiscountValue
+	drm.Couner++
+
+	if drm.Couner <= 4 {
+		return 20
+	}
+	return 0
 }
 
 // テーブルドリブンテスト
@@ -19,7 +24,6 @@ func TestDiscountCalculator(t *testing.T) {
 		name                  string
 		minimumPurchaseAmount int
 		purchaseAmount        int
-		discount              int
 		expectedAmount        int
 	}
 
@@ -28,35 +32,30 @@ func TestDiscountCalculator(t *testing.T) {
 			name:                  "should apply 20",
 			minimumPurchaseAmount: 100,
 			purchaseAmount:        150,
-			discount:              20,
 			expectedAmount:        130,
 		},
 		{
 			name:                  "should apply 40",
 			minimumPurchaseAmount: 100,
 			purchaseAmount:        200,
-			discount:              20,
 			expectedAmount:        160,
 		},
 		{
 			name:                  "should apply 60",
 			minimumPurchaseAmount: 100,
 			purchaseAmount:        350,
-			discount:              20,
 			expectedAmount:        290,
 		},
 		{
 			name:                  "should not apply",
 			minimumPurchaseAmount: 100,
 			purchaseAmount:        50,
-			discount:              20,
 			expectedAmount:        50,
 		},
 		{
 			name:                  "should not apply when discount is zero",
 			minimumPurchaseAmount: 100,
 			purchaseAmount:        50,
-			discount:              0,
 			expectedAmount:        50,
 		},
 	}
@@ -64,9 +63,7 @@ func TestDiscountCalculator(t *testing.T) {
 	for _, tc := range testCases {
 		// サブテスト
 		t.Run(tc.name, func(t *testing.T) {
-			discountRepositoryMock := DiscountRepositoryMock{
-				DiscountValue: tc.discount,
-			}
+			discountRepositoryMock := DiscountRepositoryMock{}
 			calculator, err := NewDiscountCalculator(tc.minimumPurchaseAmount, discountRepositoryMock)
 			if err != nil {
 				t.Fatalf("could not instantiate the calculator: %s", err.Error())
